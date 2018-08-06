0999be353cac52314012372390521925ab9ef642
Joe Leinsdorf <leinsdorf@gmail.com>
2018-08-06 12:06:48 -0600

    slcsp calc, need to write to csv file

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const PLANS_FILEPATH = `./plans.csv`;
 const ZIPS_FILEPATH = `./zips.csv`;
 const SLCSP_FILEPATH = `./slcsp.csv`;
 
-let parseCSVs = async function() {
+const parseCSVs = async function() {
   return {
     plans: await jsonifyCSV().fromFile(PLANS_FILEPATH),
     zips: await jsonifyCSV().fromFile(ZIPS_FILEPATH),
@@ -12,13 +12,13 @@ let parseCSVs = async function() {
   }
 }
 
-let calcSilverRatesByArea = function(plans) {
+const calcSilverRatesByArea = function(plans) {
   const silverRatesByArea = {};
   plans.forEach(plan => {
     if (plan.metal_level.toLowerCase() === `silver`) {
       const stateExists = silverRatesByArea[plan.state];
       const ratesListExists = stateExists && silverRatesByArea[plan.state][plan.rate_area];
-      if (!ratesListExists){
+      if (!ratesListExists) {
         if (!stateExists) {
           silverRatesByArea[plan.state] = {};
         }
@@ -26,15 +26,60 @@ let calcSilverRatesByArea = function(plans) {
       }
       silverRatesByArea[plan.state][plan.rate_area].push(plan.rate);
     }
-  })
+  });
   return silverRatesByArea;
 }
 
-let slcspCalc = async function() {
-  const csvData = await parseCSVs();
-  const silverRatesByArea = calcSilverRatesByArea(csvData.plans);
-  console.log(silverRatesByArea)
+const calcRatesByZip = function(silverRatesByArea, zips) {
+  const ratesByZip = {};
+  zips.forEach(zip => {
+    const stateExists = silverRatesByArea[zip.state];
+    const ratesListExists = stateExists && silverRatesByArea[zip.state][zip.rate_area];
+    if (ratesListExists) {
+      ratesByZip[zip.zipcode] = silverRatesByArea[zip.state][zip.rate_area];
+    }
+  });
+  return ratesByZip;
 }
 
-slcspCalc();
+const calcSLCSPFromRates = function(rates) {
+  let lowest, secondLowest;
+  rates.forEach(rate => {
+    if (lowest === undefined) {
+      lowest = rate;
+    } else if (rate < lowest) {
+      secondLowest = lowest;      
+      lowest = rate;
+    } else if (secondLowest === undefined || (rate < secondLowest && rate !== lowest)) {
+      secondLowest = rate;
+    }
+  });
+  return secondLowest;
+}
+
+const calcSLCSPByZip = function(ratesByZip, zips) {
+  zips.forEach(zip => {
+    const slcsp = ratesByZip[zip.zipcode] && calcSLCSPFromRates(ratesByZip[zip.zipcode]);
+    if (ratesByZip[zip.zipcode] && slcsp) {
+      zip.rate = slcsp;
+    }
+  });
+  return zips;
+}
+
+const calcSLCSP = async function() {
+  const csvData = await parseCSVs();
+  const silverRatesByArea = calcSilverRatesByArea(csvData.plans);
+  const ratesByZip = calcRatesByZip(silverRatesByArea, csvData.zips);
+  const slcspByZip = calcSLCSPByZip(ratesByZip, csvData.slcsp);
+  return slcspByZip;
+}
+
+const generateCSVSLCSP = async function() {
+  const slcspByZip = await calcSLCSP();
+  console.log(slcspByZip)
+  //write file
+}
+
+generateCSVSLCSP();
 
